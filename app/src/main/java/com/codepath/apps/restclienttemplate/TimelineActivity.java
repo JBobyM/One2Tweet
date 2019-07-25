39a7dac994fa75cac78ae643667e86a6645e1993
Kathleen <jbobyberry@gmail.com>
2019-07-25 01:42:18 -0400

    Initial commit

---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
@@ -0,0 +1,114 @@
+package com.codepath.apps.restclienttemplate;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import com.codepath.apps.restclienttemplate.models.Tweet;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import cz.msebera.android.httpclient.Header;
+
+public class TimelineActivity extends AppCompatActivity {
+    private RecyclerView rvTweets;
+    private TweetsAdapter adapter;
+    private List<Tweet> tweets;
+    private SwipeRefreshLayout swipeContainer;
+
+private TwitterClient client;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_timeline);
+
+        client = TwitterApp.getRestClient(this);
+        swipeContainer=findViewById(R.id.swipeContainer);
+        // Configure the refreshing colors
+        swipeContainer.setColorSchemeResources(android.R.color.holo_blue_bright,
+                android.R.color.holo_green_light,
+                android.R.color.holo_orange_light,
+                android.R.color.holo_red_light);
+
+
+
+        swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                Log.d("Twitterclient", "Content is being refreshed");
+                populateHomeTimeline();
+            }
+        });
+
+
+
+
+        populateHomeTimeline();
+
+        // Find the RecyclerView
+        rvTweets = findViewById(R.id.rvTweets);
+        // Initialize list of tweets from the data source
+        tweets= new ArrayList<>();
+        adapter=new TweetsAdapter(this,tweets);
+        //RecyclerView: layout manager and setup the adapter
+        rvTweets.setLayoutManager(new LinearLayoutManager(this));
+        rvTweets.setAdapter(adapter);
+    }
+
+    private void populateHomeTimeline() {
+        client.getHomeTimeLine(new JsonHttpResponseHandler(){
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
+              //  Log.d("TwitterClient", response.toString() );
+                // Iterate through the list of tweets
+                List<Tweet> tweetsToAdd =new ArrayList<>();
+                for (int i=0;i<response.length();i++){
+                    try {
+
+                        // convert each jsonobject into tweet object
+                       JSONObject jsonTweetObject =response.getJSONObject(i);
+                       Tweet tweet = Tweet.fromjson(jsonTweetObject);
+
+                        //Add the tweet into the data source
+                        tweetsToAdd.add(tweet);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+
+                    }
+                }
+                // clear existing data
+                adapter.clear();
+                // show the data we just receive
+                adapter.AddTweets(tweetsToAdd);
+                // Now we call setRefreshing(false) to signal refresh has finished
+                swipeContainer.setRefreshing(false);
+
+
+
+
+
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                Log.e("TwitterClient", errorResponse.toString() );
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                Log.e("TwitterClient", responseString );
+
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TweetsAdapter.java
@@ -0,0 +1,85 @@
+package com.codepath.apps.restclienttemplate;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.codepath.apps.restclienttemplate.models.Tweet;
+
+import java.util.List;
+
+public class TweetsAdapter extends RecyclerView.Adapter<TweetsAdapter.ViewHolder> {
+
+    private Context context;
+    private List<Tweet> Tweets;
+
+    public TweetsAdapter(Context context, List<Tweet> tweets) {
+        this.context = context;
+        Tweets = tweets;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.item_tweet, parent,false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Tweet tweet = Tweets.get(position);
+        holder.tvBody.setText(tweet.body);
+        holder.tvScreenName.setText(tweet.user.screenName);
+        Glide.with(context).load(tweet.user.profileImageUrl).into(holder.ivProfileImage);
+
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return Tweets.size();
+    }
+
+// Clean all elements of the recycler
+
+    public void clear(){
+        Tweets.clear();
+        notifyDataSetChanged();
+    }
+// Add a list of items
+    public void AddTweets(List<Tweet> tweetList){
+        Tweets.addAll(tweetList);
+        notifyDataSetChanged();
+    }
+
+    // Pass in contexts and list of tweets
+    // for each row, inflate the layout
+    // bind values based on the position of the element
+    //Define the viewholder
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        public ImageView ivProfileImage;
+        public TextView tvScreenName;
+        public  TextView tvBody;
+
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            ivProfileImage=itemView.findViewById(R.id.ivProfileImage);
+            tvScreenName = itemView.findViewById(R.id.tvScreenName);
+            tvBody=itemView.findViewById(R.id.tvBody);
+
+
+        }
+    }
+
+
+}
